fdda20ce48c25431a9508a84d8045e52e958d8df
mcolalongo <mattiacolalongo82@gmail.com>
2025-10-06 18:01:37 +0200

    shaped main, perfected class fileIO

--- FileIO.py
+++ FileIO.py
@@ -8,17 +8,15 @@ class FileIO:
     def __init__(self, file_path):
         '''
         Initialize the FileIO class with the path to the NDA file.
-        :param file_path: Path to the NDA file.
+        :param file_path: Path to the NDAx folder files.
         '''
         self.file_path = file_path
 
-    def read_data(self):
+    def load_data(self):
         '''
         Read single data from the NDA file using NewareNDA library.
         :return: Data read from the NDA file.
         '''
-        if not os.path.exists(self.file_path):
-            raise FileNotFoundError(f"The file {self.file_path} does not exist.")        
         try:
             # Read single data using NewareNDA library --> Specifically .ndax files
             data = NewareNDA.NewareNDAx.read_ndax(self.file_path)
@@ -31,20 +29,31 @@ class FileIO:
         Read multiple data sets from the NDA file using NewareNDA library.
         :return: List of data sets read from the NDA file.
         '''
-        data_list = []
+        data_list = [] # Create an empty list to store all the NDAx files
+        
         try:
-            for i in data_list:
-                data_list.append(i)
-            # Read multiple data sets using NewareNDA library --> Specifically .ndax files
+            # Loop through all the files in the specified directory and selecto only NDAx
+            for i in os.listdir(self.file_path):
+                if i.endswith('.ndax'):
+                    data_list.append(i)
+            # Cool print to see all the NDAx files in the folder
+            print(f"{bcolors.BOLD}Files in the folder:{bcolors.ENDC}")
+            [print("{} File Name {} --> {}".format(bcolors.OKCYAN,bcolors.ENDC,data_list[i])) for i in range(len(data_list))]
+            # Return the data list
             return data_list
-        except Exception as e:
-            raise IOError(f"An error occurred while reading the files in the folder: {e}")
+        
+        except:
+            raise IOError(f"An error occurred while reading the files in the folder")
         
 
-    # def save_to_csv(self, data, output_path):
-    #     try:
-    #         df = pd.DataFrame(data)
-    #         df.to_csv(output_path, index=False)
-    #         print(f"Data successfully saved to {output_path}")
-    #     except Exception as e:
-    #         raise IOError(f"An error occurred while saving to CSV: {e}")
+# Cool colors for printing in terminal
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
--- main.py
+++ main.py
@@ -4,5 +4,13 @@ from tkinter import filedialog
 
 load = filedialog.askdirectory(title="Select the folder containing the data files")
 file = FileIO(load)
-print(file)
+file_list = file.read_multiple()
+
+
+select = input("\n\nSelect the type of analysis you want to perform: \n1. ESR-C\n2. SelfD\n")
+
+if select=="SelfD":
+    print("At the moment the feature is not available")
+else:
+    print("Data Analysis Started")
 
