210dd6a01c272e71af92cb44a23ab2184edcda07
mcolalongo <mattiacolalongo82@gmail.com>
2025-10-06 16:17:49 +0200

    updated main for folder read

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-import FileIO
+from fileIO import FileIO
 import tkinter as tk
+from tkinter import filedialog
+
+load = filedialog.askdirectory(title="Select the folder containing the data files")
+file = FileIO(load)
+print(file)
 
